3df0d65337b55cd2f180b2bbe5fa974eb3f5736a
hisyam270810 <m.hisyam.darmawan.002@gmail.com>
2025-08-04 13:18:35 +0700

    Add files via upload

---
+++ perkenalan.java
@@ -0,0 +1,13 @@
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("muhammad hisyam darmawan");
+        System.out.println("SMK TELKOM MSLSNG");
+        System.out.println("SMP THURSINA IIBS");
+        System.out.println("cipung darmawan");
+        System.out.println("dini iswati");
+        System.out.println("pembalap");
+        System.out.println("berkuda");
+    }
+}
+    
+
