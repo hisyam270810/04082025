1926884733b7103da9076f29140937b75aeb5297
hisyam270810 <m.hisyam.darmawan.002@gmail.com>
2025-08-04 14:25:09 +0700

    Add files via upload

---
+++ teman.java
@@ -0,0 +1,26 @@
+public class teman {
+    public static void main(String[] args) {
+        //variabel -> tipe_data nama_variabel =value;
+        String nama1="iqbal ibrahim";
+        int usia1=15;
+        String cita1 = "pembisnis";
+        
+        String nama2="haidar ali";
+        int usia2=16;
+        String cita2="software developer";
+
+        String nama3="ramadhani putra";
+        int usia3=15;
+        String cita3="bekerja di jepang";
+
+        String nama4="ilham daniswara";
+        int usia4=15;
+        String cita4="software engginer"
+
+        String nama5="maven helios";
+        int usia5=15;
+        String cita5="web developer";
+
+
+    }
+}
